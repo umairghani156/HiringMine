1fee0f572706fb5e717caffb73f6efbcd158dcf2
umairghani156 <umairghani156@gmail.com>
2024-05-30 18:02:13 +0500

    changesCommitted

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(cors({
 app.use(cookieParser())
 dbConnection()
 const PORT = process.env.port_number || 7000
+app.use("/",(req,res)=>{
+    res.send("Chl rha hn")
+})
 
 app.use("/api/auth", authRoute)
 app.use("/api/jobs", jobsRoute)
