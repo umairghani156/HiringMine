3439fdb86dc6e8bdc021cdc5454df0cef8c91846
umairghani156 <umairghani156@gmail.com>
2024-06-11 00:26:15 +0500

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["POST","GET","PUT","DELETE"],
     withCredentials: true,
 }))
