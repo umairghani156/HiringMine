feff4f4498cefa2f79be643ef3be6120bc41021e
UmairAhmed <139696048+umairghani156@users.noreply.github.com>
2024-06-13 18:53:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config()
 app.use(express.json())
 app.use(cors({
-    origin: "https://hiringmineweb-frontend.vercel.app",
+    origin: "https://hm-frontendweb.vercel.app",
     methods: ["POST","GET","PUT","DELETE"],
     withCredentials: true,
 }))
